d7be72fe066813b70265d0344c2ec0446cf6ffd0
unknown <aminasokoli0602@outlook.com>
2025-12-30 17:53:26 +0100

    Completed the JwtService implementation

--- backend/sourdough/src/main/java/org/example/sourdough/service/users/JwtService.java
+++ backend/sourdough/src/main/java/org/example/sourdough/service/users/JwtService.java
@@ -1,11 +1,15 @@
 package org.example.sourdough.service.users;
 
 import io.jsonwebtoken.Claims;
-import org.springframework.stereotype.Service;
-
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Map;
 import java.util.function.Function;
 
 public interface JwtService {
     public <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver);
     public String extractUsername(String jwt);
+    String generateToken(UserDetails userDetails);
+    String generateToken(Map<String, Object> extraClaims, UserDetails userDetails);
+    String generateRefreshToken(UserDetails userDetails);
+    boolean isValidJwt(String jwt, UserDetails userDetails);
 }
--- backend/sourdough/src/main/java/org/example/sourdough/service/users/JwtServiceImpl.java
+++ backend/sourdough/src/main/java/org/example/sourdough/service/users/JwtServiceImpl.java
@@ -2,14 +2,23 @@ package org.example.sourdough.service.users;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService {
     private String secretKey;
     private long jwtExpiration;
+    private long refreshExpiration;
 
     public String extractUsername(String jwt) {
         return extractClaim(jwt, Claims::getSubject);
@@ -21,6 +30,47 @@ public class JwtServiceImpl implements JwtService {
     }
 
     private Claims extractAllClaims(String jwt) {
-        return Jwts.parser().setSigningKey("key").build().parseClaimsJws(jwt).getBody();
+        return Jwts.parser().setSigningKey(getSignInKey()).build().parseClaimsJws(jwt).getBody();
     }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return buildToken(extraClaims, userDetails, jwtExpiration);
+    }
+
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateRefreshToken(UserDetails userDetails) {
+        return buildToken(new HashMap<>(), userDetails, refreshExpiration);
+    }
+
+    public boolean isValidJwt(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
 }
