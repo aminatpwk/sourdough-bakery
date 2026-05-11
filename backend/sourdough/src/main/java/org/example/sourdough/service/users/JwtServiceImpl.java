a146a4ea974783293e9f3b18eb5e729b4bed516c
aminatpwk <aminasokoli0602@outlook.com>
2026-05-12 00:04:00 +0200

    fix JwtServiceImpl with @Value injection

--- backend/sourdough/src/main/java/org/example/sourdough/service/users/JwtServiceImpl.java
+++ backend/sourdough/src/main/java/org/example/sourdough/service/users/JwtServiceImpl.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +17,22 @@ import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService {
+
+    @Value("${jwt.secret-key}")
     private String secretKey;
+
+    @Value("${jwt.expiration}")
     private long jwtExpiration;
+
+    @Value("${jwt.refresh-expiration}")
     private long refreshExpiration;
 
+    @Override
     public String extractUsername(String jwt) {
         return extractClaim(jwt, Claims::getSubject);
     }
 
+    @Override
     public <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(jwt);
         return claimsResolver.apply(claims);
@@ -33,10 +42,12 @@ public class JwtServiceImpl implements JwtService {
         return Jwts.parser().setSigningKey(getSignInKey()).build().parseClaimsJws(jwt).getBody();
     }
 
+    @Override
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
+    @Override
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return buildToken(extraClaims, userDetails, jwtExpiration);
     }
@@ -56,10 +67,12 @@ public class JwtServiceImpl implements JwtService {
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
+    @Override
     public String generateRefreshToken(UserDetails userDetails) {
         return buildToken(new HashMap<>(), userDetails, refreshExpiration);
     }
 
+    @Override
     public boolean isValidJwt(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
